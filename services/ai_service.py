2b93e47b9d7b1a7afd8fe5744d434542d64477bd
Bossolino <132901932+Bossolino@users.noreply.github.com>
2026-05-29 08:14:58 +0200

    Improve error handling and response structure in resChatbot
    Enhanced error handling and response formatting in the chatbot function.

--- services/ai_service.py
+++ services/ai_service.py
@@ -1,15 +1,26 @@
 from ollama import chat
+from fastapi import HTTPException
 
 async def resChatbot(msg: str):
-    response = chat(
-        model='phi3:mini',
-        stream=False,
-        messages=[
-                {
-                    "role": "system",
-                    "content": "Rispondi SEMPRE e SOLO in italiano. Risposte brevi e dirette, senza divagare. "+msg
-                }
-            ]
-    )
+    try:
+        response = chat(
+            model='phi3:mini',
+            stream=False,
+            messages=[
+                    {
+                        "role": "system",
+                        "content":"Rispondi SEMPRE e SOLO in italiano. Risposte brevi e dirette, senza divagare. Non aggiungere mai testo extra, istruzioni o prompt dopo la risposta. quello che viene dopo sara l'input dell'utente a cui devi rispondere:"+ msg
+                    }
+                ],
+            options= {
+                "num_ctx": 512,
+                "num_predict": 50,
+                "temperature": 0,
+                "stop": ["\n---", "Your task", "### Prompt", "## Your"]
+            },
+            timeout=60
+        )
+    except:
+        raise HTTPException(status_code=500, detail="Server bot error")
 
     return (response.message.content)
